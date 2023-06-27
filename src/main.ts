27c9bdfdd4a21c243eed693e2c3753cea3da8cbf
wata189 <watarusato105@gmail.com>
2023-06-27 11:22:13 +0900

    quasar追加

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,22 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
+import { Quasar } from 'quasar';
 
-createApp(App).mount('#app')
+// Import icon libraries
+import '@quasar/extras/material-icons/material-icons.css';
+import '@quasar/extras/material-icons-outlined/material-icons-outlined.css';
+import '@quasar/extras/material-icons-round/material-icons-round.css';
+import '@quasar/extras/material-icons-sharp/material-icons-sharp.css';
+
+// Import Quasar css
+import 'quasar/src/css/index.sass';
+
+const myApp = createApp(App);
+
+myApp.use(Quasar, {
+  plugins: {}, // import Quasar plugins and add here
+});
+
+// Assumes you have a <div id="app"></div> in your index.html
+myApp.mount('#app');
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,22 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { quasar, transformAssetUrls } from '@quasar/vite-plugin';
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'multisearch-vite',
-  plugins: [vue()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  base: '/multisearch-vite/',
+  plugins: [
+    vue({
+      template: { transformAssetUrls }
+    }),
+    quasar({
+      sassVariables: 'src/quasar-variables.sass'
+    })
+  ],
+});

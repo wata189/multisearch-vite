36f902065b142e046912f7cc4ffd5665765b06f0
wata189 <watarusato105@gmail.com>
2023-06-28 12:09:41 +0900

    #2 #5 ヘッダー作成

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,39 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue';
+
+const searchWord = ref("");
 </script>
 
 <template>
-  <HelloWorld msg="Hello Multisearch!" />
+  <q-layout>
+
+    <q-header elevated class="bg-info text-white">
+      <div class="row">
+        <q-toolbar class="col-auto">
+          <q-toolbar-title>
+            マルチサーチ
+          </q-toolbar-title>
+
+        </q-toolbar>
+        <q-toolbar class="col-5">
+          <q-input
+            v-model="searchWord"
+            label-color="white"
+            label="検索ワード"
+            class="full-width"
+          >
+            <template v-slot:append>
+              <q-btn text-color="white" icon="search">検索</q-btn>
+            </template>
+          </q-input>
+        </q-toolbar>
+      </div>
+    </q-header>
+
+    <q-page-container>
+    </q-page-container>
+
+  </q-layout>
 </template>
 
 <style scoped>
